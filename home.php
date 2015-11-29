072eef6011990bfc2a87c7e7cca402eb2fefb388
Matthieu Vergne <matthieu.vergne@gmail.com>
2015-11-29 15:58:00 +0100

    Revise home description.

--- home.php
+++ home.php
@@ -1,3 +1,17 @@
 <h1>Donkey Carter Support</h1>
 
-<p>Here comes some description...</p>
+<p>
+Donkeys are part of the inhabitants of Grahamstown. Many people make a living out of them, and we can see them everywhere. Unfortunately, they can sometime be injured while wandering in the town, or being catched by animal protection organisations. After the donkeys, their owners are the first affected by such events. And by helping the owners in taking care of his donkeys, we can minimize the troubles for everyone.
+</p>
+
+<p>
+This is why FACE provides a support to donkey carters, in order to help them retrieve their donkeys. When you encounter a donkey in the street, check his <a href="https://en.wikipedia.org/wiki/QR_code">QR code</a> on your smartphone. You will get updated information about the donkey and how to contact his owner. Any relevant information will be provided immediately to you for your safety, for instance if the donkey is sick and should be avoided, or if she is pregnant and should be handled carefully.
+</p>
+
+<p>
+By providing this information easily, FACE helps donkey carters retrieve their donkeys as soon as possible! It minimises the risk for the donkey to be hurt, and for people in town to be bothered. It also diminishes the need for other organisations to intervene and catch the donkeys. All in all, it allows to increase the capability for donkey carters to take care of their donkeys, decreasing significantly the fear of losing their dear partners.
+</p>
+
+<p>
+So we hope that you will help us in this task for the sake of everyone!
+</p>
